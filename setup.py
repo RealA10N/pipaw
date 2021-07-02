c71f243010a9619d9e9ffc60feda2c1ea2b2e4cb
Alon (PC) <downtown2u@gmail.com>
2021-07-02 18:39:53 +0300

    ⚡ Updated `setup.py` details to new `pipaw`

--- setup.py
+++ setup.py
@@ -1,48 +1,40 @@
 import os.path
+from setuptools import setup, find_packages
 
 
 THIS = __file__
 HERE = os.path.dirname(THIS)
 README = os.path.join(HERE, 'README.md')
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
-
 __author__ = 'ping <lastmodified@gmail.com>'
 __version__ = '1.6.0'
 
-packages = [
-    'instagram_private_api',
-    'instagram_private_api.endpoints',
-    'instagram_web_api'
-]
 
 with open(README, encoding='utf8') as f:
     long_description = f.read()
 
 setup(
-    name='instagram_private_api',
+    name='pipaw',
     version=__version__,
-    author='ping',
-    author_email='lastmodified@gmail.com',
+    author='ping, reala10n',
+    author_email='lastmodified@gmail.com, downtown2u@gmail.com',
     license='MIT',
-    url='https://github.com/ping/instagram_private_api/tree/master',
+    url='https://github.com/reala10n/pipaw',
     install_requires=[],
-    keywords='instagram private api',
+    keywords='instagram private api bot',
     description='A client interface for the private Instagram API.',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    packages=packages,
+    packages=find_packages(),
     platforms=['any'],
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Topic :: Software Development :: Libraries :: Python Modules',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ]
 )
