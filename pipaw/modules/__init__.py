eee18f1798e123430a326c42321580b1f742bd58
Alon (PC) <downtown2u@gmail.com>
2021-07-10 13:43:25 +0300

    ✨ Added `modules/__init__.py`

---
+++ pipaw/modules/__init__.py
@@ -0,0 +1,12 @@
+from .base import InstagramModule
+from .user import InstagramUser
+from .media import InstagramPost, InstagramMedia, InstagramPhoto, InstagramVideo
+
+__all__ = [
+    'InstagramModule',
+    'InstagramUser',
+    'InstagramMedia',
+    'InstagramPost',
+    'InstagramPhoto',
+    'InstagramVideo',
+]
