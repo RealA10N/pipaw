cf69c797364098743c74ae2d40ca16c0c4c96bc5
Alon (PC) <downtown2u@gmail.com>
2021-07-10 15:17:14 +0300

    ✨ Added the `user` property to `InstagramPost`

--- pipaw/modules/media.py
+++ pipaw/modules/media.py
@@ -18,6 +18,16 @@ class InstagramPost(modules.InstagramModule):
     def link(self,) -> str:
         return f'https://www.instagram.com/p/{self.code}/'
 
+    @property
+    def user(self,) -> 'modules.InstagramUser':
+        """ Returns an `InstagramUser` instance that represents the user that
+        uploaded the post. """
+
+        return modules.InstagramUser(
+            self._api,
+            initial_data=self._data.get('user'),
+        )
+
 
 class InstagramMedia:
     pass
