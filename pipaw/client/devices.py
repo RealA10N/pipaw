d3f0b0a3bb2b756d76ce1d2f7e6272e2445102c7
Alon (PC) <downtown2u@gmail.com>
2021-07-20 16:36:06 +0300

    ✨ Added new devices and app version to the database

--- pipaw/client/devices.py
+++ pipaw/client/devices.py
@@ -1,5 +1,12 @@
 import typing
+from random import choice
 from dataclasses import dataclass
+
+__all__ = [
+    'AppVersion',
+    'UserDevice',
+    'InstagramUserAgent',
+]
 
 
 @dataclass(frozen=True)
@@ -8,17 +15,14 @@ class AppVersion:
     that indicates the app (by default, the app name is `Instagram`). Here are
     a couple of version strings for example:
 
-    -   Instagram 195.0.0.31.123 Android
-    -   Instagram 196.0.0.0.54 Android
-    -   Instagram 76.0.0.15.395 Android
+    -   Instagram 195.0.0.31.123
+    -   Instagram 196.0.0.0.54
+    -   Instagram 76.0.0.15.395
     """
 
     version: typing.Tuple[int, int, int, int, int]
     """ A tuple representing the version and containing 5 different
     integers. """
-
-    os: str = 'Android'
-    """ The os name, `Android` by default. """
 
     name: str = 'Instagram'
     """ The app name, `Instagram` by default. """
@@ -34,7 +38,7 @@ class AppVersion:
         """ A string representation of the app version.
         This string representation is used as a part of the *user agent*. """
 
-        return f'{self.name} {self.version_str} {self.os}'
+        return f'{self.name} {self.version_str}'
 
 
 @dataclass(frozen=True)
@@ -55,9 +59,9 @@ class UserDevice:
     purpose is to generate a *user-agent* string that will be acceptable by
     the Instagram private API. """
 
-    app: AppVersion
-    android_version: int
-    android_release: int
+    os: str
+    os_version: int
+    os_release: int
     dpi: int
     resolution: PhoneResolution
     brand: str
@@ -67,18 +71,13 @@ class UserDevice:
     version_code: int
     language: str = 'en_US'
 
-    def __str__(self,) -> str:
-        """ Generates and returns a *user-agent* string that represents the
-        current device. A couple examples of valid user-agent string are:
-
-        -   Instagram 195.0.0.31.123 Android (29/10; 420dpi; 1080x2181; samsung;
-            SM-N960W; crownqltecs; qcom; en_US; 302733750)
-        -   Instagram 76.0.0.15.395 Android (24/7.0; 640dpi; 1440x2560; samsung;
-            SM-G930F; herolte; samsungexynos8890; en_US; 138226743)
-        """
+    @property
+    def details(self,) -> str:
+        """ A string that contains additional information about the device.
+        The info is provided as a list of values, seperated by ';'. """
 
         parts = (
-            f'{self.android_version}/{self.android_release}',
+            f'{self.os_version}/{self.os_release}',
             f'{self.dpi}dpi',
             self.resolution,
             self.brand,
@@ -89,22 +88,178 @@ class UserDevice:
             self.version_code,
         )
 
-        return f"{self.app!s} ({'; '.join(str(p) for p in parts)})"
+        return '; '.join(str(p) for p in parts)
 
+    def __str__(self,) -> str:
+        """ Generates and returns a *user-agent* string that represents the
+        current device. A couple examples of valid user-agent string are:
+
+        -   Android (29/10; 420dpi; 1080x2181; samsung; SM-N960W; crownqltecs;
+            qcom; en_US; 302733750)
+        -   Android (24/7.0; 640dpi; 1440x2560; samsung; SM-G930F; herolte;
+            samsungexynos8890; en_US; 138226743)
+        """
+
+        return f"{self.os!s} ({self.details})"
+
+
+""" A list of valid Instagram app versions, as `AppVersion` instances. App
+version list retrived from https://www.apk4fun.com/history/869/ """
+VERSIONS = (
+    AppVersion((195, 0, 0, 31, 123)),   # July 2021 👇
+    AppVersion((194, 0, 0, 36, 172)),   # June 2021 👇
+    AppVersion((193, 0, 0, 45, 120)),
+    AppVersion((192, 0, 0, 35, 123)),
+    AppVersion((191, 1, 0, 41, 124)),
+    AppVersion((190, 0, 0, 36, 119)),
+    AppVersion((189, 0, 0, 41, 121)),   # May 2021 👇
+    AppVersion((188, 0, 0, 35, 124)),
+    AppVersion((187, 0, 0, 32, 120)),
+    AppVersion((186, 0, 0, 36, 128)),
+    AppVersion((185, 0, 0, 38, 116)),   # April 2021 👇
+    AppVersion((183, 0, 0, 40, 116)),
+    AppVersion((182, 0, 0, 29, 124)),
+    AppVersion((181, 0, 0, 33, 117)),   # March 2021 👇
+    AppVersion((180, 0, 0, 31, 119)),
+    AppVersion((179, 0, 0, 31, 132)),
+    AppVersion((178, 1, 0, 37, 123)),
+    AppVersion((177, 0, 0, 30, 119)),
+)
 
 DEVICES = (
     UserDevice(
-        # Samsung Note S9 - August 2018
-        app=AppVersion((195, 0, 0, 31, 123)),
-        android_version=29,
-        android_release=10,
-        dpi=420,
-        resolution=PhoneResolution(1080, 2118),
+        # Samsung Galaxy Note S9 - August 2018
         brand='samsung',
         device='SM-N960W',
+        os='Android',
+        os_version=29,
+        os_release=10,
+        dpi=420,
+        resolution=PhoneResolution(1080, 2118),
         model='crownqltecs',
         chipset='qcom',
-        language='en_US',
         version_code=302733750,
     ),
+    UserDevice(
+        # Samsung Galaxy S8+ - April 2017
+        # https://developers.whatismybrowser.com/useragents/parse/47317249instagram-android-sm-g955u-webkit
+        brand='samsung',
+        device='SM-G955U',
+        os='Android',
+        os_version=28,
+        os_release=9,
+        dpi=420,
+        resolution=PhoneResolution(1080, 2094),
+        model='dream2qltesq',
+        chipset='qcom',
+        version_code=236572377,
+    ),
+    UserDevice(
+        # Samsung Galaxy A10e - June 2019
+        # https://developers.whatismybrowser.com/useragents/parse/50001905instagram-android-sm-a102u-webkit
+        brand='samsung',
+        device='SM-A102U',
+        os='Android',
+        os_version=28,
+        os_release=9,
+        dpi=320,
+        resolution=PhoneResolution(720, 1468),
+        model='a10e',
+        chipset='exynos7885',
+        version_code=239490550,
+    ),
+    UserDevice(
+        # Samsung Galaxy S9 - March 2018
+        # https://developers.whatismybrowser.com/useragents/parse/49856852instagram-android-sm-g960u-webkit
+        brand='samsung',
+        device='SM-G960U',
+        os='Android',
+        os_version=28,
+        os_release=9,
+        dpi=480,
+        resolution=PhoneResolution(1080, 2076),
+        model='starqltesq',
+        chipset='qcom',
+        version_code=240726484,
+    ),
+    UserDevice(
+        # Samsung Galaxy Note10+ - August 2019
+        # https://developers.whatismybrowser.com/useragents/parse/50535487instagram-android-sm-n975u-webkit
+        brand='samsung',
+        device='SM-N975U',
+        os='Android',
+        os_version=29,
+        os_release=10,
+        dpi=480,
+        resolution=PhoneResolution(1080, 2051),
+        model='d2q',
+        chipset='qcom',
+        version_code=206670927,
+    ),
+    UserDevice(
+        # Motorola One - August 2018
+        # https://developers.whatismybrowser.com/useragents/parse/1221393instagram-android-webkit
+        brand='motorola',
+        device='motorola one',
+        os='Android',
+        os_version=27,
+        os_release=8,
+        dpi=320,
+        resolution=PhoneResolution(720, 1362),
+        model='deen_sprout',
+        chipset='qcom',
+        version_code=132081645,
+    ),
+    UserDevice(
+        # Samsung Galaxy J7 Prime - September 2016
+        # https://developers.whatismybrowser.com/useragents/parse/1218090instagram-android-sm-g610m-webkit
+        brand='samsung',
+        device='SM-G610M',
+        os='Android',
+        os_version=23,
+        os_release=6,
+        dpi=480,
+        resolution=PhoneResolution(1080, 1920),
+        model='on7xelte',
+        chipset='samsungexynos7870',
+        version_code=103516666,
+    ),
+    UserDevice(
+        # Samsung Galaxy S10 - February 2019
+        # https://developers.whatismybrowser.com/useragents/parse/60168765instagram-android-sm-g973f-webkit
+        brand='samsung',
+        device='SM-G973F',
+        os='Android',
+        os_version=29,
+        os_release=10,
+        dpi=420,
+        resolution=PhoneResolution(1080, 2042),
+        model='beyond1',
+        chipset='exynos9820',
+        version_code=256099204,
+    ),
 )
+
+
+@dataclass
+class InstagramUserAgent:
+    """ A dataclass for generating random Instagram valid *user-agent* strings
+    and storing them. """
+
+    app: AppVersion
+    device: UserDevice
+
+    def __str__(self,) -> str:
+        """ Generates a valid Instagram *user-agent* and returns it as a
+        string. """
+
+        return f'{self.app!s} {self.device!s}'
+
+    @staticmethod
+    def random() -> 'InstagramUserAgent':
+        """ Generates and returns a random `InstagramUserAgent` instance. """
+
+        return InstagramUserAgent(
+            app=choice(VERSIONS),
+            device=choice(DEVICES),
+        )
